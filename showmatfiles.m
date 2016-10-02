91263825811cd2172ae4b64a4d8e77aa15a42cea
Dalton Burke <dmtburke@gmail.com>
2016-10-01 23:07:10 -0600

    testing some functions

--- showmatfiles.m
+++ showmatfiles.m
@@ -15,16 +15,23 @@ d=dir('*.mat');d={d.name};
 output = [];
 
 for i=1:length(d)
-
     load(d{i});
     
-    % These operate under the assumption that the biggest value will
-    % always appear in the top left and the lowest in the bottom right
     upperLong = max(u.long(:));
     lowerLong = min(u.long(:));
     
+    if(upperLong - lowerLong > 180)
+        upperLong = -180;
+        lowerLong = 180;
+    end
+    
     upperLat  = max(u.lat(:));
     lowerLat  = min(u.lat(:));
+    
+    if upperLat - lowerLat > 180
+        upperLat = -180;
+        lowerLat = 180;
+    end
    
     longinRange = lonlat(1) < upperLong && lonlat(2) > lowerLong;
     latinRange  = lonlat(3) < upperLat  && lonlat(4) > lowerLat;
@@ -34,7 +41,7 @@ for i=1:length(d)
         geoshow(u.lat, u.long, u.data, cmapmod14)
         hold on
         
-        output{length(output)+1} = d{i};
+        output{length(output)+1} = [d{i} ', uLong: ' upperLong ', lLong: ' lowerLong ', uLat: ' upperLat ', lLat: ' lowerLat];
     else
         disp(['Skipping ' d{i} '...']);
     end
